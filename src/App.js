6dc6bea3973eef0111202c20eb4b0e4d7d81bebd
iamanasmo <ammamasz.basha@gmail.com>
2022-03-10 12:26:18 +0200

    main

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Apps } from "@mui/icons-material/";
+const App = () => {
+  return (
+    <div>
+      <h2>hello2</h2>
+    </div>
+  );
+};
+
+export default App;
